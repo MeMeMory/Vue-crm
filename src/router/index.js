3c46c264251db9c5ac581cca1b8ee3d258d58cb4
alex.kostushevich <alex.kostushevich@ithoot.com>
2022-01-14 16:34:45 +0200

    add base vue templates, update router

--- src/router/index.js
+++ src/router/index.js
@@ -1,14 +1,57 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
-import Home from "../views/Home.vue";
 
 Vue.use(VueRouter);
 
 const routes = [
   {
-    path: "/",
-    name: "Home",
-    component: Home,
+    path: "/Login",
+    name: "Login",
+    meta: { layout: "empty" },
+    component: () => import("../views/Login.vue"),
+  },
+  {
+    path: "/Registration",
+    name: "Registration",
+    meta: { layout: "empty" },
+    component: () => import("../views/Register.vue"),
+  },
+  {
+    path: "/Categories",
+    name: "Categories",
+    meta: { layout: "main" },
+    component: () => import("../views/Categories.vue"),
+  },
+  {
+    path: "/Detail-record",
+    name: "Detail-record",
+    meta: { layout: "main" },
+    component: () => import("../views/Detail-record.vue"),
+  },
+  {
+    path: "/History",
+    name: "History",
+    meta: { layout: "main" },
+    component: () => import("../views/History.vue"),
+  },
+  {
+    path: "/Planing",
+    name: "Planing",
+    meta: { layout: "main" },
+    component: () => import("../views/Planning.vue"),
+  },
+
+  {
+    path: "/Profile",
+    name: "Profile",
+    meta: { layout: "main" },
+    component: () => import("../views/Profile.vue"),
+  },
+  {
+    path: "/Record",
+    name: "Record",
+    meta: { layout: "main" },
+    component: () => import("../views/Record.vue"),
   },
 ];
 
---
+++ src/views/History.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="history-chart">
+    <canvas></canvas>
+  </div>
+</template>
